caad8668aea748497d90a3c82e2a6b5ddb32c6b3
João Pablo <jp.pablolima@gmail.com>
2021-09-10 17:17:18 -0300

    transações sql

---
+++ transacoes.sql
@@ -0,0 +1,15 @@
+SELECT numero, nome, ativo 
+FROM banco 
+ORDER BY numero;
+
+UPDATE banco SET ativo = false WHERE numero = 0; 
+
+BEGIN;
+UPDATE banco SET ativo = true WHERE  numero = 0;
+SELECT numero, nome, ativo FROM banco WHERE numero = 0;
+ROLLBACK;
+
+
+BEGIN;
+SELECT numero, nome, ativo FROM banco WHERE numero = 0;
+COMMIT;
